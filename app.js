7ec8806a306b6759e3a950641b067b49ce5149e5
James Cotton <jamescotton.email@gmail.com>
2021-10-13 21:58:53 +0100

    removed extraneous code

--- app.js
+++ app.js
@@ -1,32 +1,4 @@
-// fetch('https://api.cryptonator.com/api/ticker/btc-usd')
-//     .then(res => {
-//         console.log("RESPONSE, WAITING TO PARSE...", res)
-//         return res.json()
-//     })
-//     .then(data => {
-//         console.log("DATA PARSED...")
-//         console.log(data.ticker.price)
-//     })
-//     .catch(e => {
-//         console.log("OH NO! ERROR!", e)
-//     })
 
-// axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
-//     .then(res => {
-//         console.log(res.data.ticker.price)
-//     })
-//     .catch(err => {
-//         console.log("ERROR!", err)
-//     })
-
-const fetchBitcoinPrice = async () => {
-    try {
-        const res = await axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
-        console.log(res.data.ticker.price)
-    } catch (e) {
-        console.log("ERROR!", e)
-    }
-}
 
 const jokes = document.querySelector('#jokes');
 const button = document.querySelector('button');
